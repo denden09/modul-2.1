ba25c2ab3e6b8c92ad5a86b821d5b2709896c290
Vinsensius Arden <zeroxleashgaming@gmail.com>
2025-02-12 21:49:29 +0700

    upt step 5

--- app/src/main/java/com/example/reply/ui/theme/Theme.kt
+++ app/src/main/java/com/example/reply/ui/theme/Theme.kt
@@ -1,13 +1,13 @@
 package com.example.reply.ui.theme
 
 import android.app.Activity
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -82,8 +82,15 @@ fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    // Define color scheme
-    val colors = if (useDarkTheme) DarkColors else LightColors
+    val context = LocalContext.current
+    val colors = when {
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
+            if (useDarkTheme) dynamicDarkColorScheme(context)
+            else dynamicLightColorScheme(context)
+        }
+        useDarkTheme -> DarkColors
+        else -> LightColors
+    }
 
     // Add primary status bar color from chosen color scheme.
     val view = LocalView.current
@@ -93,7 +100,7 @@ fun AppTheme(
             window.statusBarColor = colors.primary.toArgb()
             WindowCompat
                 .getInsetsController(window, view)
-                .isAppearanceLightStatusBars = !useDarkTheme
+                .isAppearanceLightStatusBars = useDarkTheme
         }
     }
 
